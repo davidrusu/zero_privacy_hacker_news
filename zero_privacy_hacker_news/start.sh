908634df284a403665bde760ec3c4367b726d3a6
David Rusu <davidrusu.me@gmail.com>
2016-03-31 16:46:40 -0400

    remove the old rails server PID file

--- zero_privacy_hacker_news/start.sh
+++ zero_privacy_hacker_news/start.sh
@@ -1,12 +1,11 @@
 #! /usr/bin/env bash
 
-set -e
-
 PID_FILE="tmp/pids/$HOSTNAME.pid"
 
 if [ -e $PID_FILE ]; then
     echo "Killing old server"
     kill -9 $(cat $PID_FILE)
+    rm $PID_FILE
 fi
 
 rails server -p 80 -b '0.0.0.0' -e production -P $PID_FILE
